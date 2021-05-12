940da4cd60b6ac3439244f06d0c6784242474c09
antoni0caldeira <antoniocaldeira@yahoo.com>
2021-05-12 15:47:45 +0100

    Create a data model

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from './user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  userSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionOption: 'annual',
+    notes: 'here are some notes'
+  }
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/user-settings-form/user-settings.ts
@@ -0,0 +1,7 @@
+export interface UserSettings{
+    name: string,
+    emailOffers: boolean,
+    interfaceStyle: string,
+    subscriptionOption: string,
+    notes: string
+}

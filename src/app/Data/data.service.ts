63ebc7f6877fc0c9f4140d64e70df5aacb163b3e
antoni0caldeira <antoniocaldeira@yahoo.com>
2021-05-13 15:03:17 +0100

    Creating a data service

---
+++ src/app/Data/data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { UserSettings } from '../user-settings-form/user-settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+  postUserSettingsForm(userSettings: UserSettings){
+    
+  }
+}
